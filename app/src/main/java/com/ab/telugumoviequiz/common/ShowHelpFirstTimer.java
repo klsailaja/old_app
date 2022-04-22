0adc25a93def90ff36bec151cd7669f372b72ac8
Rajasekhar G <ggraj.pec@gmail.com>
2022-04-22 09:30:58 +0530

    Bug Fixes

--- app/src/main/java/com/ab/telugumoviequiz/common/ShowHelpFirstTimer.java
+++ app/src/main/java/com/ab/telugumoviequiz/common/ShowHelpFirstTimer.java
@@ -1,2 +1,28 @@
-package com.ab.telugumoviequiz.common;public class ShowHelpFirstTimer {
+package com.ab.telugumoviequiz.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShowHelpFirstTimer {
+    private static ShowHelpFirstTimer instance;
+    private Map<String, Integer> firstTimeStatus = new HashMap<>();
+
+    private ShowHelpFirstTimer() {
+    }
+
+    public static ShowHelpFirstTimer getInstance() {
+        if (instance == null) {
+            instance = new ShowHelpFirstTimer();
+        }
+        return instance;
+    }
+
+    public boolean isFirstTime(String helpViewKey) {
+        Integer state = firstTimeStatus.get(helpViewKey);
+        if (state == null) {
+            firstTimeStatus.put(helpViewKey, 1);
+            return true;
+        }
+        return false;
+    }
 }
